cadd50c0291682b573014a53ff1d5dcfe3293fb2
salahelec2 <salahelec2@gmail.com>
2023-06-08 14:04:49 -0700

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     arg_num = len(argv) - 1
     if arg_num < 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
