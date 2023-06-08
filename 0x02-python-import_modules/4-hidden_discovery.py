138e6eeae0d496d086caf7ca5e4f1cbba8a0a3c5
salahelec2 <salahelec2@gmail.com>
2023-06-08 12:30:25 -0700

    hidden_discovery add test to __name__

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4
 
-names = dir(hidden_4)
+    names = dir(hidden_4)
 
-for nm in names:
-    if nm[:2] == "__":
-        continue
-    else:
-        print(nm)
+    for nm in names:
+        if nm[:2] == "__":
+            continue
+        else:
+            print(nm)
