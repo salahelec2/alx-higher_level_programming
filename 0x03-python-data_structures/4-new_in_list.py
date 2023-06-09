1a1f1942eb849099f00cb1cf59a9fc73a6f3d265
salahelec2 <salahelec2@gmail.com>
2023-06-09 15:46:28 -0700

    5th task created

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if len(my_list) - 1 < idx or idx < 0:
+        return my_list
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
