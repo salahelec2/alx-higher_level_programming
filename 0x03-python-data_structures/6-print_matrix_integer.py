932c452ee742a7ba602c3d7ab2def9794ae5fa08
salahelec2 <salahelec2@gmail.com>
2023-06-09 16:01:05 -0700

    7th task pycodestyle resolved

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for elem in matrix:
-        print("{}".format(" ".join("{:d}".format(element) for element in elem)))
+        print("{}".format(" ".join("{}".format(element) for element in elem)))
