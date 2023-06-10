420ed9a50724d5d33fb7140598b2abcd9d963816
salahelec2 <salahelec2@gmail.com>
2023-06-10 13:43:15 -0700

    tuples add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     max_length = max(len(tuple_a), len(tuple_b))
-    new_tuple = tuple((tuple_a[i] if i < len(tuple_a) else 0) + \
-                      (tuple_b[i] if i < len(tuple_b) else 0) \
-                        for i in range(max_length))
-    return new_tuple
+    new_tuple = []
+    for i in range(max_length):
+        element_a = tuple_a[i] if i < len(tuple_a) else 0
+        element_b = tuple_b[i] if i < len(tuple_b) else 0
+        sum_of_elements = element_a + element_b
+        new_tuple.append(sum_of_elements)
+    return tuple(new_tuple)
