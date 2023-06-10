83c34444ba8441260b261152a05f79b386fe8334
salahelec2 <salahelec2@gmail.com>
2023-06-10 12:24:27 -0700

    return max val in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
 def max_integer(my_list):
-    old_val = 0
+    if len(my_list) == 0:
+        return None  # Return None for an empty list
+    old_val = my_list[0]
     for i in my_list:
         if i > old_val:
             old_val = i
