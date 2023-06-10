75a904ddb49dd25637a5bf7b4335d8df006d86d6
salahelec2 <salahelec2@gmail.com>
2023-06-10 12:15:01 -0700

    multiple returns

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     max_length = max(len(tuple_a), len(tuple_b))
-    new_tuple = tuple((tuple_a[i] if i < len(tuple_a) else 0) + (tuple_b[i] if i < len(tuple_b) else 0) for i in range(max_length))
+    new_tuple = tuple((tuple_a[i] if i < len(tuple_a) else 0) + \
+                      (tuple_b[i] if i < len(tuple_b) else 0) \
+                        for i in range(max_length))
     return new_tuple
