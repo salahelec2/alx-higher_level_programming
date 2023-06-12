7176b579504c972d23578caae325a96dfbfa3117
salahelec2 <salahelec2@gmail.com>
2023-06-12 14:57:17 -0700

    c code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,73 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int count = 0;
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
-	current = h;
-	while (current != NULL)
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		count++;
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
 	}
 
-	return count;
+	*head = prev;
+	return (*head);
 }
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+int is_palindrome(listint_t **head)
 {
-	listint_t *new_node;
-	listint_t *current_node;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	current_node = *head;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return NULL;
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (*head == NULL)
-		*head = new_node;
-	else
+	tmp = *head;
+	while (tmp)
 	{
-		while (current_node->next != NULL)
-			current_node = current_node->next;
-		current_node->next = new_node;
+		size++;
+		tmp = tmp->next;
 	}
 
-	return new_node;
-}
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current_node;
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
 
-	while (head != NULL)
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		current_node = head;
-		head = head->next;
-		free(current_node);
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
 	}
+	reverse_listint(&mid);
+
+	return (1);
 }
