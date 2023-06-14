6d93c003ac631bee6fdf0af994d2721b3397677b
salahelec2 <salahelec2@gmail.com>
2023-06-14 16:56:14 -0700

    update new dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dictionary = dict()
-    for elment in a_dictionary:
-        new_dictionary.update({elment : a_dictionary[elment]})
+    new_dictionary = a_dictionary
     new_dictionary.update({key : value})
     return new_dictionary
