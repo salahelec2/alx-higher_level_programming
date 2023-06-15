aaf7fa222f034139f5080a5d3633d6cbc90417fa
salahelec2 <salahelec2@gmail.com>
2023-06-14 17:14:19 -0700

    max val in dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    return max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == max(a_dictionary.values()):
+            return key
