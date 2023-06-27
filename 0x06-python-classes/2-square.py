fb158b04240c21188d8614800bcc4f77f7fe83f9
salahelec2 <salahelec2@gmail.com>
2023-06-27 16:43:33 -0700

    task 1

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""CLASS SQUARE DEFINITION"""
+
+
+class Square:
+    """the square class body"""
+
+    def __init__(self, size):
+        """new Square init .
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
