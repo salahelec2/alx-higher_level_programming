5785079a86bb7460fff00c03e37ae827d789878e
salahelec2 <salahelec2@gmail.com>
2023-06-27 16:41:41 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""CLASS SQUARE DEFINITION"""
+
 
 class Square:
+    """the square class body"""
+
     def __init__(self, size):
+        """new Square init .
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
