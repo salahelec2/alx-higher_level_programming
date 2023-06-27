78b4a4c27d5eaec1a58af65f6a95883a12c71efb
salahelec2 <salahelec2@gmail.com>
2023-06-26 17:30:40 -0700

    first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     number = 0
-    i = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
             number += 1
-        except:
+        except IndexError:
             continue
     print()
     return number
+
