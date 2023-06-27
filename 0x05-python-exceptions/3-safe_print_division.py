ab668e709a5cb5417933cb9e6cb05aa9509c0982
salahelec2 <salahelec2@gmail.com>
2023-06-26 17:55:07 -0700

    task 3 checker resolved

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ValueError:
         result = None
     finally:
         print("Inside result: {}".format(result))
